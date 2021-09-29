66942fe59a89536fd6e1212b8af05fc565975c7b
kzssdsm1 <obaddonsoul@gmail.com>
2021-09-29 20:55:46 +0900

    first commit

---
+++ CalcApp_sadoshima/CalcApp_sadoshima/CalcApp_sadoshimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp_sadoshimaApp.swift
+//  CalcApp_sadoshima
+//
+//  Created by 佐渡島和志 on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp_sadoshimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcApp_sadoshima/CalcApp_sadoshima/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalcApp_sadoshima
+//
+//  Created by 佐渡島和志 on 2021/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
