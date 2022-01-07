fc9a5ea496cd30effa60815e0f1634aa0f4b849a
kzssdsm1 <obaddonsoul@gmail.com>
2022-01-07 13:48:12 +0900

    一部機能を追加

--- CalcApp_sadoshima/CalcApp_sadoshima/ViewModel/ConverterViewModel.swift
+++ CalcApp_sadoshima/CalcApp_sadoshima/ViewModel/ConverterViewModel.swift
@@ -30,8 +30,8 @@ final class ConverterViewModel: ObservableObject {
     
     // 税込み→税抜き
     func calcNonTaxedPrice(_ num: NSDecimalNumber) -> NSDecimalNumber {
-        let diviser = NSDecimalNumber(string: "1.1")
-        let divided = num.dividing(by: diviser)
+        let divider = NSDecimalNumber(string: "1.1")
+        let divided = num.dividing(by: divider)
         let rounded = divided.rounding(accordingToBehavior: behavior)
         
         return rounded
@@ -40,9 +40,9 @@ final class ConverterViewModel: ObservableObject {
     // 税込み→消費税
     func calcTax(_ num: NSDecimalNumber) -> NSDecimalNumber {
         let multiplier = NSDecimalNumber(string: "0.1")
-        let diviser = NSDecimalNumber(string: "1.1")
+        let divider = NSDecimalNumber(string: "1.1")
         let multiplied = num.multiplying(by: multiplier)
-        let divided = multiplied.dividing(by: diviser)
+        let divided = multiplied.dividing(by: divider)
         let rounded = divided.rounding(accordingToBehavior: behavior)
         
         return rounded
@@ -51,9 +51,9 @@ final class ConverterViewModel: ObservableObject {
     // 華氏→摂氏
     func toCelsius(_ num: NSDecimalNumber) -> NSDecimalNumber {
         let subtructer = NSDecimalNumber(string: "32")
-        let diviser = NSDecimalNumber(string: "2")
+        let divider = NSDecimalNumber(string: "2")
         let subtructed = num.subtracting(subtructer)
-        let divided = subtructed.dividing(by: diviser)
+        let divided = subtructed.dividing(by: divider)
         let behavior = NSDecimalNumberHandler(
             roundingMode: NSDecimalNumber.RoundingMode.plain,
             scale: 1,
@@ -82,6 +82,94 @@ final class ConverterViewModel: ObservableObject {
             raiseOnDivideByZero: false
         )
         let rounded = added.rounding(accordingToBehavior: behavior)
+        
+        return rounded
+    }
+    
+    // 身長(cm)→適正体重
+    func toAverageWeight(_ num: NSDecimalNumber) -> NSDecimalNumber {
+        let divider = NSDecimalNumber(string: "10")
+        let divided = num.dividing(by: divider)
+        let behavior = NSDecimalNumberHandler(
+            roundingMode: NSDecimalNumber.RoundingMode.plain,
+            scale: 2,
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+        let rounded = divided.rounding(accordingToBehavior: behavior)
+        let multiplied = rounded.multiplying(by: rounded)
+        let multiplier = NSDecimalNumber(string: "22")
+        let averageWeight = multiplied.multiplying(by: multiplier)
+        
+        return averageWeight
+    }
+    
+    // cm→inch
+    func cmToInch(_ num: NSDecimalNumber) -> NSDecimalNumber {
+        let multiplier = NSDecimalNumber(string: "0.39370079")
+        let multiplied = num.multiplying(by: multiplier)
+        let behavior = NSDecimalNumberHandler(
+            roundingMode: NSDecimalNumber.RoundingMode.down,
+            scale: 2,
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+        let rounded = multiplied.rounding(accordingToBehavior: behavior)
+        
+        return rounded
+    }
+    
+    // inch→cm
+    func inchToCm(_ num: NSDecimalNumber) -> NSDecimalNumber {
+        let multiplier = NSDecimalNumber(string: "2.54")
+        let multiplied = num.multiplying(by: multiplier)
+        let behavior = NSDecimalNumberHandler(
+            roundingMode: NSDecimalNumber.RoundingMode.down,
+            scale: 2,
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+        let rounded = multiplied.rounding(accordingToBehavior: behavior)
+        
+        return rounded
+    }
+    
+    // cm→寸
+    func cmToSun(_ num: NSDecimalNumber) -> NSDecimalNumber {
+        let multiplier = NSDecimalNumber(string: "0.33")
+        let multiplied = num.multiplying(by: multiplier)
+        let behavior = NSDecimalNumberHandler(
+            roundingMode: NSDecimalNumber.RoundingMode.down,
+            scale: 2,
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+        let rounded = multiplied.rounding(accordingToBehavior: behavior)
+        
+        return rounded
+    }
+    
+    // 寸→cm
+    func sunToCm(_ num: NSDecimalNumber) -> NSDecimalNumber {
+        let multiplier = NSDecimalNumber(string: "3.0303")
+        let multiplied = num.multiplying(by: multiplier)
+        let behavior = NSDecimalNumberHandler(
+            roundingMode: NSDecimalNumber.RoundingMode.down,
+            scale: 2,
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+        let rounded = multiplied.rounding(accordingToBehavior: behavior)
         
         return rounded
     }
