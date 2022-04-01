291b8d0bedc95aa5bafecbd260a8cbdb79f9d509
kzssdsm1 <obaddonsoul@gmail.com>
2022-04-01 14:27:12 +0900

    数値監視用のclassを作成

---
+++ CalcApp_sadoshima/CalcApp_sadoshima/Combine/NumberObserver.swift
@@ -0,0 +1,14 @@
+//
+//  NumberObserver.swift
+//  CalcApp_sadoshima
+//
+//  Created by 佐渡島和志 on 2022/04/01.
+//
+
+import Foundation
+
+final class NumberObserver {
+    static let shared = NumberObserver()
+    
+    private init() {}
+}
