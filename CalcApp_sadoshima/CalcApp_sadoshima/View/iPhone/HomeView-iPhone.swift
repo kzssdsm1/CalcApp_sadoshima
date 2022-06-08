f4cdf947927d25d705fb5f049efc03834f1e11e9
kzssdsm1 <obaddonsoul@gmail.com>
2022-06-08 16:27:07 +0900

    画面構成用のファイルを作成

---
+++ CalcApp_sadoshima/CalcApp_sadoshima/View/iPhone/HomeView-iPhone.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView-iPhone.swift
+//  CalcApp_sadoshima
+//
+//  Created by 佐渡島和志 on 2022/06/08.
+//
+
+import SwiftUI
+
+struct HomeView_iPhone: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
